fb575f63b2846f070a80ea4b62f8f2223410c2e1
Karlie <jiaqix4@student.unimelb.edu.au>
2021-09-30 19:25:48 +0800

    deployment test

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 require('dotenv').config()    // for JWT password key
 
