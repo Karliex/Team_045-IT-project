0cb7370b358fcbe7bb8630f2699d3e5b9f4e75fe
HUAZHENH <61773483+HUAZHENH@users.noreply.github.com>
2021-10-20 19:42:03 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const path = require('path');
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 const app = express();
 const server = require('http').createServer(app);
 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
 // parses data received from POST and makes available the request body
 app.use(express.json())
@@ -18,10 +18,8 @@ app.use(cors())
 dotenv.config()
 mongoose.connect(process.env.DATABASE_ACCESS, () =>console.log("Database connected"))
 
-
 // modules for authentication
 const passport = require('passport');
-
 const session = require('express-session');
 const flash = require('connect-flash');
 const jwt = require('jsonwebtoken');
@@ -37,20 +35,22 @@ app.use(
     saveUninitialized: true
 }));
 
-
 app.use(express.static('/client'));
 
 // Passport middleware
-app.use(passport.initialize());       // initialize usage of passport
-app.use(passport.session());          // invoked when using login session
-app.use(flash());                    // Connect flash
+// initialize usage of passport
+app.use(passport.initialize());       
+// invoked when using login session
+app.use(passport.session());   
+// Connect flash
+app.use(flash());                   
 
 // Global variables
 app.use(function (req, res, next) {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
-  next();
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
 });
 
 // Use the routers with path
@@ -59,15 +59,13 @@ app.use('/user', userRoute);
 
 // for heroku deployment
 if (process.env.NODE_ENV === 'production') {
-
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 server.listen(port, () =>
-	console.log('Server listening for requests ...'))
+    console.log('Server listening for requests ...'))
 
 module.exports = app;
